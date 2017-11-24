2b520527a0fae3a53a7a95c0b6b135091df11f63
admin <admin@Solehin-MacBook-Pro.local>
2017-11-24 20:01:34 +0800

    testis

---
+++ PHP Files/DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database username here.
+$HostUser = "id3723291_khairul";
+
+//Define your database password here.
+$HostPass = "123456";
+
+//Define your database name here.
+$DatabaseName = "id3723291_khairul";
+
+?>
